d9fb7e39defcb57050adb4377b68568591d3ddb2
SirStrategic <luca.colarusso@sydstu.catholic.edu.au>
2023-12-03 09:37:39 +1100

    gen1 mostly complete
    main.py functional

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ random.seed(None, 2)
 #create vareints: power 1, negative power, negative root, negative root & exponential, positive power, positive root, positive root and power
 #create Gen1, put everything below into a function so that a list can be spit out for later use
 def GenBox():
-    boxCode = [[0,0],1,1,0,0,["x",0]]
+    boxCode = [[0,0],1,1,0,0,["x",1]]
     gen1list = []
     for i in range(100): #100 is a placeholder
 
@@ -18,7 +18,12 @@ def GenBox():
         boxCode[2] = random.randint(1, 9)
         boxCode[3] = random.randint(1, 9)
         boxCode[4] = random.randint(0, 8)
-        boxCode[5][1] = random.randint(1,9)
+
+        if boxCode[1] == 0 :
+            boxCode[1] = 1
+
+        if (boxCode[1]/0.5)%2 == 0:
+            boxCode[1] = int(boxCode[1])
 
         if boxCode[4] == 0:
             boxdash = Basic.power(boxCode, ["",1])
@@ -31,7 +36,7 @@ def GenBox():
         elif boxCode[4] == 4:
             boxdash = Exponentials.exponential(boxCode, ["",1])
         elif boxCode[4] == 5:
-            boxdash = fraction_logs.Logarithm(boxCode, ["",1])
+            boxdash = fraction_logs.logaraithm(boxCode, ["",1])
         elif boxCode[4] == 6:
             boxdash = fraction_inverse_trig.arcsin(boxCode, ["",1])
         elif boxCode[4] == 7:
@@ -43,7 +48,7 @@ def GenBox():
         result = interpret(boxCode)
         gen1list.append([result, boxdash])
         print(f"{result[1]}{result[0]}", boxdash)
-        return gen1list
+    return gen1list
 
 
 GenBox()
