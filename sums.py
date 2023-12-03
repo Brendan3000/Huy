20ceee4de99ca32bd11deb6b06bfa611a961faa8
D3mo1ish3r <152149792+D3mo1ish3r@users.noreply.github.com>
2023-12-03 22:12:25 +1100

    Update sums.py

--- sums.py
+++ sums.py
@@ -44,4 +44,41 @@ class Sum: #UNTESTED
 
         return [returnbox, returnboxdash]
 
+#ideally checks if result is an integer, ie. box_v ==0 and adds the values if result is an integer (99% sure  DOESNT WORK) 
+def sums (box_v, box_c):
+  result = box_v + box_c
+  if isinstance(result, int):
+    print(result)   
+  else:
+    print(sum(result))
 
+# List of common factors to check for factorising 
+def factor_check([box_c]{box_v}):
+    factors = [
+        f'sin([box_c]{box_v})', f'ln([box_c]{box_v})', f'tan([box_c]{box_v})', f'cos([box_c]{box_v})',
+        f'arccos([box_c]{box_v})', f'arcsin([box_c]{box_v})', f'arctan([box_c]{box_v})', f'e**([box_c]{box_v})', f'x**([box_c]{box_v})', f'(base)**([box_c]{box_v})
+    ]
+
+    for factor in factors:
+        if factor in str([box_c]{box_v}):
+            return factor
+
+    return None
+  
+def factorize([box_c]{box_v}):
+    factors = [
+        f'sin([box_c]{box_v})', f'ln([box_c]{box_v})', f'tan([box_c]{box_v})', f'cos([box_c]{box_v})',
+        f'arccos([box_c]{box_v})', f'arcsin([box_c]{box_v})', f'arctan([box_c]{box_v})', f'e**([box_c]{box_v})', f'x**([box_c]{box_v})'
+    ]
+
+    for factor in factors:
+        if factor in str([box_c]{box_v}):
+            common_factor = factor
+  # Attempting to factorize by dividing the expression by the common factor
+            expression = str([box_c]{box_v}).replace(common_factor, '')  
+            if expression.startswith('*'):  
+                expression = expression[1:] 
+
+            return expression.strip()  
+    
+    return str([box_c]{box_v})  
