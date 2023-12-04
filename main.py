a6680818449f1e9f43f37b42cd1e0e3830a52bb1
SirStrategic <luca.colarusso@sydstu.catholic.edu.au>
2023-12-04 22:26:39 +1100

    RECRUSION HELL
    Imma have to take the plunge... Tomorow

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from interperter import interpret
 import random
 random.seed(None, 2)
 
-#create vareints: power 1, negative power, negative root, negative root & exponential, positive power, positive root, positive root and power
-#create Gen1, put everything below into a function so that a list can be spit out for later use
+#below may incorrect
+#after generating one 'function' I can chose to do 4 things, 1 encapsulate it further using another function, 2 put in a sum, 3 product, 4 quotient. 2,3,4 all require me to fill in the remaining spots
+#I will fillin the remaining spots by doing an identical generation, however to avoid infinite bullshit, i will pass down a variable limiting the number of recalls which when = 0 will just
+hardcap = 4 # this is the limiting variable's initial value mentioned above, currently set to 1, to be manualy changed. UPDATE, THIS NOW IS A GENERAL DISTANCE FROM ORIGINAL POINT, I'LL JUST INCREAS ETHE CHANCE OF GETTING 1 LOL lolLLOOLLOL
+maxsumsize = 3
 
-def GenerateBoxCode(innerbox): #innerbox is in form ["value", "derivative"] and is passed straight through
+def GenerateBoxCode(innerbox): #innerbox is in form ["value", "derivative"] and is passed straight through, inner box is what rests in place of "x"
     boxCode = [[0, 0], 1, 1, 0, 0, [innerbox[0], 1]]
     dividor = random.randint(1, 2)
     boxCode[0][0] = random.randint(0, 1)
@@ -25,47 +28,68 @@ def GenerateBoxCode(innerbox): #innerbox is in form ["value", "derivative"] and 
 
     return [boxCode, innerbox]
 
+#find a way so genbox can call genfullfunction and etc https://stackoverflow.com/questions/71473962/how-do-i-make-functions-call-each-other god help me
+def GenBox(masterFuncType, box, boxdash): #pass in variable 1,2,3,4 see above, if 2,3,4 then ignore the specified function and place it into a sum,product,quotient ||| cap is current value of hardcap, reduces with each 2,3,4 called
 
-def GenBox():
+    if currentdeapth == 0:
+        masterFuncType = 1
 
-    gen1list = []
-    deapth = 1
-    for i in range(1): #100 is a placeholder
+    if masterFuncType == 1:
 
         #previous values of [box str, boxdash str]
-        boxAndDash = GenerateBoxCode(["x^2", "2x"])
+        boxAndDash = GenerateBoxCode([box, boxdash])
+
+            if boxAndDash[0][4] == 0:
+                boxdash = Basic.power(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 1:
+                boxdash = Basic.sin(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 2:
+                boxdash = Basic.cos(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 3:
+                boxdash = Basic.tan(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 4:
+                boxdash = Exponentials.exponential(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 5:
+                boxdash = fraction_logs.logaraithm(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 6:
+                boxdash = fraction_inverse_trig.arcsin(boxAndDash[0], [boxAndDash[1][1],1])
+            elif boxAndDash[0][4] == 7:
+                boxdash = fraction_inverse_trig.arccos(boxAndDash[0], [boxAndDash[1][1],1])
+            else: #tan^-1
+                boxdash = fraction_inverse_trig.arcctan(boxAndDash[0], [boxAndDash[1][1],1])
+
+                result = interpret(boxAndDash[0])
+                return [None, currentdeapth - 1, f"(({result[1]})({result[0]}))", f"((l({boxdash[0][0]})/({boxdash[0][1]}))({boxdash[1]}))"]
+
+    elif masterFuncType == 2: #sum
+        #call this function n times more
+        GenBox(random, currentdeapth-1, 'x', '1')
+    elif masterFuncType == 3: #product
+        print("Wrong way : Turn Back") #not implemented yet
+    elif masterFuncType == 4: #quotient
+        print("Wrong way : Turn Back") #not implemented yet
+
+    return 0
 
 
 
-        if boxAndDash[0][4] == 0:
-            boxdash = Basic.power(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 1:
-            boxdash = Basic.sin(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 2:
-            boxdash = Basic.cos(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 3:
-            boxdash = Basic.tan(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 4:
-            boxdash = Exponentials.exponential(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 5:
-            boxdash = fraction_logs.logaraithm(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 6:
-            boxdash = fraction_inverse_trig.arcsin(boxAndDash[0], [boxAndDash[1][1],1])
-        elif boxAndDash[0][4] == 7:
-            boxdash = fraction_inverse_trig.arccos(boxAndDash[0], [boxAndDash[1][1],1])
-        else: #tan^-1
-            boxdash = fraction_inverse_trig.arcctan(boxAndDash[0], [boxAndDash[1][1],1])
+print(GenBox())
+currentDeapth = hardcap
+#commence generation
+GenBoxOut = GenBox(1, currentDeapth, "x", "1")
 
+#make this a function where you request n depth of code and it returns a box and boxdash as strings, this'll allow recursion
+def GenFullFunction(depth):
+    for i in range(depth):
 
-        result = interpret(boxAndDash[0])
-        gen1list.append([result, boxdash])
-
-        print(f"{result[1]}{result[0]}", boxdash)
-    return gen1list
-
-
-GenBox()
+    return
 
 
 
 
+
+
+
+
+
+#make powers more common
