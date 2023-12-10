8dc0b0728277a96699e52a9d4af26fd4fb03c244
Brendan3000 <mabbuttb@gmail.com>
2023-12-10 20:33:53 +1100

    I

--- number_theory.py
+++ number_theory.py
@@ -3,16 +3,20 @@ import fractions
 import products
 
 
+# will distribute a fractional box_c to the numerator box_v and denominator box_v
 def float_to_fraction(box):
     box_v, box_c = box[0], box[1]
     numerator, denominator = quotients.splitter(box_v)
     box_c = str(fractions.Fraction(box_c))
     if "/" in box_c:
+        # Where box_c is a fraction
         box_c_numerator = products.return_number(box_c[:box_c.find("/")])
         box_c_denominator = products.return_number(box_c[box_c.find("/")+1:])
     else:
+        # Where box_c is just a integer
         box_c_numerator = products.return_number(box_c)
         box_c_denominator = ""
+    # To avoid any 1box or -box
     if box_c_numerator == 1 and numerator != "":
         box_c_numerator = ""
     if box_c_numerator == -1 and numerator != "":
@@ -24,26 +28,32 @@ def float_to_fraction(box):
     return [quotients.assembler(numerator,f"{box_c_denominator}{denominator}"), box_c_numerator]
 
 
+# input is base and exponent (floats/integers); will return True is base^exponent is rational
 def is_power_rational(base, exponent):
     box_c = str(fractions.Fraction(base))
     if "/" in box_c:
+        # where base is a fraction
         base_numerator = products.return_number(box_c[:box_c.find("/")])
         base_denominator = products.return_number(box_c[box_c.find("/")+1:])
+        # we require both top and bottom to be rational for the whole result to be rational
         if is_a_integer(base_numerator**exponent) and is_a_integer(base_denominator**exponent):
             return True
     else:
+        # where base is a integer
         base_numerator = products.return_number(box_c)
         if is_a_integer(base_numerator**exponent):
             return True
     return False
 
 
+# Input is a float/integer. If this float/integer is a integer (e.g. 5 or 5.000) it will return true
 def is_a_integer(number):
     string = str(number)
     if not "." in string:
         return True
     else:
         integer = True
+        # To test some .00000 case
         for letter in string[string.find(".")+1:]:
             if letter != "0":
                 integer = False
--- powers.py
+++ powers.py
@@ -2,28 +2,37 @@ import products
 from Nice import Brackets
 
 
-# used to convert a list containing exponent and coefficient into e^(coefficient(exponent))
+# list contains exponet (string) and a coefficient (float/integer). power is a float, string or integer. will return e raised to the product of these three inputs.
 def exponential_component(list, power):
     exponent,coefficient = list[0], list[1]
+    # case where there is a null exponential
     if exponent == "":
         return ""
     else:
         try:
-            coefficient *= products.return_number(power)
-            if coefficient == 1:
-                coefficient = ""
-            if coefficient == -1:
-                coefficient = "-"
-            return f"e^({coefficient}{exponent}) "
+            # where power is a integer/float
+            power = return_number(power)
+            coefficient *= power
         except:
-            return f"e^({coefficient}{products.multiply_two_together(power, exponent, False)}) "
+            # where power is a string
+            exponent = multiply_two_together(power,exponent, False)
+        # to avoid 1box or -1box
+        if coefficient == 1:
+            coefficient = ""
+        if coefficient == -1:
+            coefficient = "-"
+        return f"e^({coefficient}{exponent}) "
 
 
-# converts some f(x)^(ag(x)), power into f(x)^(power*ag(x))
+# converts some f(x)^(ag(x)), power into f(x)^((power*a)g(x))
 def special_assembler(box_v, power):
+    # since box^0 = 1
+    if power == 0:
+        return ""
     for index in range(len(box_v)):
         if box_v[index] == "^" and not products.is_closed_in(box_v[index:]):
             break
+    # index is the position os ^ that seperates box_base (bottom) for box_power (top)
     bottom = box_v[:index]
     top = box_v[index+2:box_v.rfind(")")]
     index_counter = 0
@@ -32,15 +41,19 @@ def special_assembler(box_v, power):
             index_counter += 1
         else:
             break
+    # index_counter denotes the length of the factor a in af(x)
     if index_counter == 0:
         constant = 1
     else:
         constant = products.return_number(top[:index_counter])
+    # To make top just a variable
     top = top[index_counter:]
     try:
+        # if power is a number
         power = products.return_number(power)
         constant *= power
     except:
+        # if power is a box_c
         top = products.multiply_two_together(power, top, False)
     if constant == 1:
         constant = ""
@@ -49,12 +62,15 @@ def special_assembler(box_v, power):
     return f"{bottom}^({constant}{top})"
 
 
-# used to distribute power across. e.g. (cos(x)sin(x))^power = coss(x)^power sin(x)^power
+# used to distribute power across. e.g. (cos(x)sin(x))^power = cos(x)^power sin(x)^power
 def power_distributor(box_v, change_power_factor):
+    # since 1^n = 1
     if len(box_v) == 0:
         return box_v
+    # since box^1 = box
     if change_power_factor == 1:
         return box_v
+    # since box^0 = 1
     if change_power_factor == 0:
         return ""
     box_v, exponential_list = products.exponetial_remover(box_v, True)
@@ -62,11 +78,14 @@ def power_distributor(box_v, change_power_factor):
     factors = []
     powers = []
     special = []
+    # To break the box into it's factors (multples) and eahc of their respective powers. special list donotes if we have a box^box case
     while len(box_v) != 0:
         while box_v[0] == " ":
             box_v = box_v[1:]
+            # to break loop if required
             if len(box_v) == 0:
                 break
+        # to break loop if required
         if len(box_v) == 0:
             break
         if box_v[0] == "x":
@@ -75,6 +94,7 @@ def power_distributor(box_v, change_power_factor):
         else:
             factor = box_v[:products.next_closed_bracket(box_v)+1]
             box_v, power, is_base_power, possible_factor = products.power_and_remover(box_v, products.next_closed_bracket(box_v))
+        # To deal with box^box case
         if is_base_power:
             factor = possible_factor
             special.append(True)
@@ -83,19 +103,23 @@ def power_distributor(box_v, change_power_factor):
         factors.append(factor)
         powers.append(power)
     for i in range(len(powers)):
+        # For box^box case
         if special[i]:
             factors[i] = special_assembler(factors[i], change_power_factor)
         else:
             try:
+                # if change_power_factor is a number
                 change_power_factor = products.return_number(change_power_factor)
                 powers[i] *= change_power_factor
             except:
+                # if change_power_factor is a variable
                 if powers[i] == 1:
                     powers[i] = ""
                 if powers[i] == -1:
                     powers[i] = "-"
                 powers[i] = f"({powers[i]}{change_power_factor})"
     for k in range(len(factors)):
+        # since box^0 = 1
         if powers[k] == 0:
             pass
         else:
